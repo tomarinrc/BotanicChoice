854ea3a66597cfef0aeb92d06cca4415e0e33a08
tomarinrc <tomarinrc@gmail.com>
2017-03-22 16:39:04 -0500

    More Changes

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -1,26 +1,41 @@
 (function($) {
-
-
-    //Sidebar Toggle
-    $('a.navbar-mobile-menu').on('click', function () {
-        $('.sidebar-overlay').toggleClass('show');
-        $('aside').toggleClass('show');
-    });
-    $('.sidebar-overlay').on('click', function () {
-        $('.sidebar-overlay').removeClass('show');
-        $('aside').removeClass('show');
-    });
-
-    //Mobile Hero
-    var position = 0;
-    var hero_position = $('.hero figure').css("left");
-    $('.hero figure').on('swipeleft', tinder);
-
-    function tinder() {
-
-        if (hero_position != "400vw") {
-            position += 100;
-            $('.hero figure').css("left", postion + 'vw');
-        }
-    }
+	//Navbar Related Scripts
+	$(".promo").stick_in_parent();
+	//Sidebar Toggle
+	$('a.navbar-mobile-menu').on('click', function() {
+		$('.sidebar-overlay').toggleClass('show');
+		$('aside').toggleClass('show');
+	});
+	$('.sidebar-overlay').on('click', function() {
+		$('.sidebar-overlay').removeClass('show');
+		$('aside').removeClass('show');
+	});
+	$('aside.sidebar').swipe({
+		swipeLeft: function() {
+			$('.sidebar-overlay').removeClass('show');
+			$('aside').removeClass('show');
+		}
+	});
+	$('.sidebar-overlay').swipe({
+		swipe: function() {
+			$('.sidebar-overlay').removeClass('show');
+			$('aside').removeClass('show');
+		}
+	});
+	//Mobile Hero
+	var hero_position = 0;
+	$('.hero figure').swipe({
+		swipeLeft: function() {
+			if (hero_position > -4) {
+				hero_position -= 1;
+				$('.hero figure').css("left", String(hero_position * 100) + 'vw');
+			}
+		},
+		swipeRight: function() {
+			if (hero_position < 0) {
+				hero_position += 1;
+				$('.hero figure').css("left", String(hero_position * 100) + 'vw');
+			}
+		}
+	});
 })(jQuery);
