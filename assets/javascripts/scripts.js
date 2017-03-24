2673e3a38e28ac0f17967a394d6479097e1bea60
tomarinrc <tomarinrc@gmail.com>
2017-03-24 16:42:05 -0500

    Basically Finished Mobile and Semi-Finished Tablet. Started Working on Sidebar Polish.

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -1,29 +1,13 @@
-var hero = {	
-	position : 0,
-	node : $('.hero figure'),
-	MoveLeft : function () {
-		if (this.position > -4) {
-			this.position -= 1;
-			this.node.css("left", String(this.position * 100) + 'vw');
-		}
-	},
-	MoveRight : function () {
-		if (this.position < 0) {
-			this.position += 1;
-			this.node.css("left", String(this.position * 100) + 'vw');
-		}
-	},
-};
-
 (function($) {
 	//Navbar Related Scripts
 	$('a.navbar-mobile-search').on('click', function() {
 		$('.promo').toggleClass('show');
 		$('.search-mobile').toggleClass('show');
 	});
-	//Sidebar Toggle
+	//Sidebar Related Scripts
 	$('a.navbar-mobile-menu').on('click', function() {
 		$('.sidebar-overlay').toggleClass('show');
+		$('aside.sidebar').scrollTop($(".sidebar-welcome").offset().top);
 		$('aside').toggleClass('show');
 	});
 	$('.sidebar-overlay').on('click', function() {
@@ -42,57 +26,72 @@ var hero = {
 			$('aside').removeClass('show');
 		}
 	});
-	//Mobile Hero
-	/*var hero_position = 0;
-	$('.hero figure').swipe({
-		swipeLeft: function() {
-			if (hero_position > -4) {
-				hero_position -= 1;
-				$('.hero figure').css("left", String(hero_position * 100) + 'vw');
-			}
-		},
-		swipeRight: function() {
-			if (hero_position < 0) {
-				hero_position += 1;
-				$('.hero figure').css("left", String(hero_position * 100) + 'vw');
-			}
-		}
-	});
-	$('.hero .fa-chevron-left').on('click', function() {
-		if (hero_position < 0) {
-			hero_position += 1;
-			$('.hero figure').css("left", String(hero_position * 100) + 'vw');
-		}
-	});
-	$('.hero .fa-chevron-right').on('click', function() {
-		if (hero_position > -4) {
-			hero_position -= 1;
-			$('.hero figure').css("left", String(hero_position * 100) + 'vw');
-		}
-	});*/
 	
-	/*var hero = {	
+	//Promo Banner Related Scripts
+	$('header .promo i.fa-times').click(function () {
+		$('section.hero').css("margin-top", $('.navbar-mobile').height());
+		$('header .promo').remove();
+	})
+	
+	//Hero Banner Related Scripts
+	var hero = {	
 		position : 0,
 		node : $('.hero figure'),
+		UpdateBullets : function () {
+			$('.hero-nav a.active').removeClass('active');
+			$('.hero-nav a.slide-' + Math.abs(this.position)).addClass('active');
+		},
 		MoveLeft : function () {
 			if (this.position > -4) {
 				this.position -= 1;
-				hero.node.css("left", String(this.position * 100) + 'vw');
+				this.node.css("left", String(this.position * 100) + 'vw');
+				this.UpdateBullets();
+			}
+			else {
+				this.position = 0;
+				this.node.css("left", String(this.position * 100) + 'vw');
+				this.UpdateBullets();
 			}
 		},
 		MoveRight : function () {
 			if (this.position < 0) {
 				this.position += 1;
-				hero.node.css("left", String(this.position * 100) + 'vw');
+				this.node.css("left", String(this.position * 100) + 'vw');
+				this.UpdateBullets();
+			}
+			else {
+				this.position = -4;
+				this.node.css("left", String(this.position * 100) + 'vw');
+				this.UpdateBullets();
 			}
 		},
-	};*/
+		AutoScroll : function () {
+			hero.MoveLeft();
+		},
+	};
 	
 	hero.node.swipe({
 		swipeLeft: function () { hero.MoveLeft(); },
 		swipeRight: function () { hero.MoveRight(); }
 	});
+	
 	$('.hero .fa-chevron-left').click(function() { hero.MoveRight(); });
 	$('.hero .fa-chevron-right').click(function() { hero.MoveLeft(); });
 	
+	setInterval(hero.AutoScroll, 7500);
+	
 })(jQuery);
+
+
+/*var myVar;
+
+function myFunction() {
+    myVar = setInterval(alertFunc, 10000);
+}
+
+function alertFunc() {
+    hero.MoveLeft();
+}
+
+myFunction();
+*/
